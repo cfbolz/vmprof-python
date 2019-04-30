bcacd131e2cbdea4490b285691f484dcfca47c80
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2019-04-30 13:57:54 +0200

    switch computation of start and stop times

--- vmprof/test/test_flamechart.py
+++ vmprof/test/test_flamechart.py
@@ -1,12 +1,16 @@
+import pytest
 
-def coalesce(profiles, stepsize=1, skew=0):
+def coalesce(profiles):
     if not profiles:
         return []
     output = []
     stack_with_start_times = []
+    start_time = profiles[0][1]
+    last_time = profiles[-1][1] + 1
 
-    for timestep, profile in enumerate(profiles + [([], )]):
+    for profile in profiles + [([], last_time)]:
         sampled_stack = profile[0]
+        curr_time = profile[1] - start_time
 
         # find index up to where two consecutive stack samples agree
         minindex = min(len(sampled_stack), len(stack_with_start_times))
@@ -18,23 +22,16 @@ def coalesce(profiles, stepsize=1, skew=0):
                     break
             else:
                 max_matching_index += 1
-        size_skew = len(stack_with_start_times) - max_matching_index - 1
 
         # the functions that are on the last sampled stack but not on the
         # current have finished, output them
         for j in range(max_matching_index, len(stack_with_start_times)):
-            end = timestep * stepsize - skew * size_skew
-            output.append(stack_with_start_times.pop() + (end, ))
-            size_skew -= 1
-        assert size_skew == -1
+            output.append(stack_with_start_times.pop() + (curr_time, ))
         assert len(stack_with_start_times) == max_matching_index
 
         # the functions that are new on the current stack have just started
-        size_skew = 0
         for j in range(max_matching_index, len(sampled_stack)):
-            start_time = timestep * stepsize + skew * size_skew
-            stack_with_start_times.append((sampled_stack[j], start_time))
-            size_skew += 1
+            stack_with_start_times.append((sampled_stack[j], curr_time))
         assert len(stack_with_start_times) == len(sampled_stack)
     output.sort(key=lambda x: (x[1], x[1] - x[2], x[0]))
     return output
@@ -60,51 +57,51 @@ def test_coalesce():
     assert r == [(1, 0, 1), (2, 0, 1)]
 
     profiles = [([1, 2], 1, 1),
-                ([1, 2], 1, 1)]
+                ([1, 2], 2, 1)]
     r = coalesce(profiles)
     assert r == [(1, 0, 2), (2, 0, 2)]
 
     profiles = [([1, 2], 1, 1),
-                ([1, 3], 1, 1)]
+                ([1, 3], 2, 1)]
     r = coalesce(profiles)
     assert r == [(1, 0, 2), (2, 0, 1), (3, 1, 2)]
 
     profiles = [([1], 1, 1),
-                ([1, 2], 1, 1),
-                ([1], 1, 1)]
+                ([1, 2], 2, 1),
+                ([1], 3, 1)]
     r = coalesce(profiles)
     assert r == [(1, 0, 3), (2, 1, 2)]
 
     profiles = [([1], 1, 1),
-                ([1, 2], 1, 1),
-                ([3], 1, 1)]
+                ([1, 2], 2, 1),
+                ([3], 3, 1)]
     r = coalesce(profiles)
     assert r == [(1, 0, 2), (2, 1, 2), (3, 2, 3)]
 
     profiles = [([1], 1, 1),
-                ([1, 2], 1, 1),
-                ([3, 2], 1, 1)]
+                ([1, 2], 2, 1),
+                ([3, 2], 3, 1)]
     r = coalesce(profiles)
     assert r == [(1, 0, 2), (2, 1, 2), (2, 2, 3), (3, 2, 3)]
 
-def test_stepsize():
-    profiles = [([1], 1, 1),
-                ([1, 2], 1, 1),
-                ([3], 1, 1)]
-    r = coalesce(profiles, 100)
-    assert r == [(1, 0, 200), (2, 100, 200), (3, 200, 300)]
+def test_larger_steps():
+    profiles = [([1], 0, 1),
+                ([1, 2], 5, 1),
+                ([3], 10, 1)]
+    r = coalesce(profiles)
+    assert r == [(1, 0, 10), (2, 5, 10), (3, 10, 11)]
 
-def test_skew():
-    profiles = [([1, 2], 1, 1),
-                ([1, 2], 1, 1)]
-    r = coalesce(profiles, 100, 1)
-    assert r == [(1, 0, 200), (2, 1, 199)]
+def test_even_more():
+    profiles = [([1, 2], 0, 1),
+                ([1, 2], 10, 1)]
+    r = coalesce(profiles)
+    assert r == [(1, 0, 11), (2, 0, 11)]
 
-    profiles = [([1], 1, 1),
-                ([1, 2, 3, 4, 5], 1, 1),
-                ([1, 2, 6], 1, 1),
-                ([1, 7, 8], 1, 1)]
-    r = coalesce(profiles, 100, 1)
-    assert r == [(1, 0, 400), (2, 100, 300), (3, 101, 200), (4, 102, 199),
-                 (5, 103, 198), (6, 200, 299), (7, 300, 399), (8, 301, 398)]
+    profiles = [([1], 0, 1),
+                ([1, 2, 3, 4, 5], 10, 1),
+                ([1, 2, 6], 15, 1),
+                ([1, 7, 8], 20, 1)]
+    r = coalesce(profiles)
+    assert r == [(1, 0, 21), (2, 10, 20), (3, 10, 15), (4, 10, 15),
+                 (5, 10, 15), (6, 15, 20), (7, 20, 21), (8, 20, 21)]
 
