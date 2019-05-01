8d0d492d4c4b5b81a353f557c03751406eedc83c
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2019-05-01 16:11:33 +0200

    use middle of two samples as start and end points

--- vmprof/test/test_flamechart.py
+++ vmprof/test/test_flamechart.py
@@ -8,11 +8,16 @@ def coalesce(profiles, factor=1, skew=0):
     output = []
     stack_with_start_times = []
     start_time = profiles[0][1]
-    last_time = profiles[-1][1] + 1
+    last_time = profiles[-1][1] + 2
+    prev_sample = 0
 
     for profile in profiles + [([], last_time)]:
         sampled_stack = profile[0]
-        curr_time = profile[1] - start_time
+        curr_sample = profile[1] - start_time
+
+        # estimate start time of functions in the middle between the previous
+        # sample and the current one
+        curr_time = (curr_sample + prev_sample) // 2
 
         # find index up to where two consecutive stack samples agree
         minindex = min(len(sampled_stack), len(stack_with_start_times))
@@ -42,6 +47,7 @@ def coalesce(profiles, factor=1, skew=0):
             stack_with_start_times.append((sampled_stack[j], start))
             size_skew += 1
         assert len(stack_with_start_times) == len(sampled_stack)
+        prev_sample = curr_sample
     output.sort(key=lambda x: (x[1], x[1] - x[2], x[0]))
     return output
 
@@ -76,35 +82,41 @@ def test_coalesce():
     r = coalesce(profiles)
     assert r == [(1, 0, 2), (2, 0, 2)]
 
-    profiles = [([1, 2], 1, 1),
+    profiles = [([1, 2], 0, 1),
                 ([1, 3], 2, 1)]
     r = coalesce(profiles)
-    assert r == [(1, 0, 2), (2, 0, 1), (3, 1, 2)]
+    assert r == [(1, 0, 3), (2, 0, 1), (3, 1, 3)]
 
-    profiles = [([1], 1, 1),
+    profiles = [([1], 0, 1),
                 ([1, 2], 2, 1),
-                ([1], 3, 1)]
+                ([1], 4, 1)]
     r = coalesce(profiles)
-    assert r == [(1, 0, 3), (2, 1, 2)]
+    assert r == [(1, 0, 5), (2, 1, 3)]
 
-    profiles = [([1], 1, 1),
+    profiles = [([1], 0, 1),
                 ([1, 2], 2, 1),
-                ([3], 3, 1)]
+                ([3], 4, 1)]
     r = coalesce(profiles)
-    assert r == [(1, 0, 2), (2, 1, 2), (3, 2, 3)]
+    assert r == [(1, 0, 3), (2, 1, 3), (3, 3, 5)]
 
-    profiles = [([1], 1, 1),
+    profiles = [([1], 0, 1),
                 ([1, 2], 2, 1),
-                ([3, 2], 3, 1)]
+                ([3, 2], 4, 1)]
     r = coalesce(profiles)
-    assert r == [(1, 0, 2), (2, 1, 2), (2, 2, 3), (3, 2, 3)]
+    assert r == [(1, 0, 3), (2, 1, 3), (2, 3, 5), (3, 3, 5)]
+
+def test_coalesce_start_at_middle():
+    profiles = [([1, 2], 0, 1), ([1, 3], 2, 1), ([1, 3], 5, 1)]
+    r = coalesce(profiles)
+    assert r == [(1, 0, 6), (2, 0, 1), (3, 1, 6)]
+
 
 def test_larger_steps():
     profiles = [([1], 0, 1),
-                ([1, 2], 5, 1),
-                ([3], 10, 1)]
+                ([1, 2], 6, 1),
+                ([3], 12, 1)]
     r = coalesce(profiles)
-    assert r == [(1, 0, 10), (2, 5, 10), (3, 10, 11)]
+    assert r == [(1, 0, 9), (2, 3, 9), (3, 9, 13)]
 
 def test_even_more():
     profiles = [([1, 2], 0, 1),
@@ -114,15 +126,15 @@ def test_even_more():
 
     profiles = [([1], 0, 1),
                 ([1, 2, 3, 4, 5], 10, 1),
-                ([1, 2, 6], 15, 1),
-                ([1, 7, 8], 20, 1)]
+                ([1, 2, 6], 16, 1),
+                ([1, 7, 8], 22, 1)]
     r = coalesce(profiles)
-    assert r == [(1, 0, 21), (2, 10, 20), (3, 10, 15), (4, 10, 15),
-                 (5, 10, 15), (6, 15, 20), (7, 20, 21), (8, 20, 21)]
+    assert r == [(1, 0, 23), (2, 5, 19), (3, 5, 13), (4, 5, 13), (5, 5, 13),
+                 (6, 13, 19), (7, 19, 23), (8, 19, 23)]
 
 def test_skew():
     profiles = [([1], 0, 1),
                 ([1, 2, 3, 4], 100, 1),
                 ([1, 2], 300, 1)]
     r = coalesce(profiles, skew=1)
-    assert r == [(1, 0, 301), (2, 100, 300), (3, 101, 300), (4, 102, 299)]
+    assert r == [(1, 0, 301), (2, 50, 300), (3, 51, 200), (4, 52, 199)]
